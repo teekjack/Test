39e44de8b6cdc5ae0768e4826ef4d1bb4a86ffb6
Pratik Mukerji <pratik@pratikmukerji.com>
2011-08-09 18:59:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cars::Application.config.secret_token = '665d674bebd4ebf3c80f8bce67a837960641acb35e954954c6f553cd3fd7965c1fb0528ca45a746ee3b4c5b7481aba40cbd156ecbf6ac5c344b477e30a60619a'
